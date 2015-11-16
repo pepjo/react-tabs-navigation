8b94764ee959264c70e18ee2b5ee867bd8a2d3ca
Pep Rodeja <pepjo26@gmail.com>
2015-11-16 17:52:58 +0100

    almost finish web

--- javascripts/src/example.jsx
+++ javascripts/src/example.jsx
@@ -15,30 +15,123 @@ let component = (
         // Fist tab
         children: () => (
           <div>
-            This is the first tab content
+            <h2>Documentation and examples</h2>
+            <p>See github:
+              <a href="https://github.com/pepjo/react-tabs-navigation">
+                {'https://github.com/pepjo/react-tabs-navigation'}
+              </a>
+            </p>
           </div>
         ),
-        displayName: 'Tab 1'
+        displayName: 'Docs'
       },
       {
         // Second tab
         children: () => (
           <div>
-            This is the second tab content
+            <h2>
+              <a
+                aria-hidden="true"
+                className="anchor"
+                href="#mit-license"
+                id="mit-license">
+                <span className="octicon octicon-link"></span>
+              </a>
+              MIT License
+            </h2>
+            <p>{'Copyright (c) 2015, Pep Rodeja'}</p>
+
+            <p>{'Permission is hereby granted, free of charge, to any person obtaining a copy ' +
+            'of this software and associated documentation files (the "Software"), to deal ' +
+            'in the Software without restriction, including without limitation the rights ' +
+            'to use, copy, modify, merge, publish, distribute, sublicense, and/or sell ' +
+            'copies of the Software, and to permit persons to whom the Software is ' +
+            'furnished to do so, subject to the following conditions:'}</p>
+
+          <p>{'The above copyright notice and this permission notice shall be included in ' +
+            'all copies or substantial portions of the Software.'}</p>
+
+          <p>{'THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR ' +
+            'IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, ' +
+            'FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE ' +
+            'AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER ' +
+            'LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, ' +
+            'OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN ' +
+            'THE SOFTWARE.'}</p>
           </div>
         ),
-        displayName: 'Tab 2'
+        displayName: 'License'
       },
       {
         // Third tab
         children: () => (
-          <div>
-            This is the third tab content
-          </div>
+          <ul>
+            <li>This</li>
+            <li>is</li>
+            <li>super</li>
+            <li>large</li>
+            <li>so</li>
+            <li>you</li>
+            <li>can</li>
+            <li>scroll</li>
+            <li>down</li>
+            <li>.</li>
+            <li>This</li>
+            <li>is</li>
+            <li>super</li>
+            <li>large</li>
+            <li>so</li>
+            <li>you</li>
+            <li>can</li>
+            <li>scroll</li>
+            <li>down</li>
+            <li>.</li>
+            <li>This</li>
+            <li>is</li>
+            <li>super</li>
+            <li>large</li>
+            <li>so</li>
+            <li>you</li>
+            <li>can</li>
+            <li>scroll</li>
+            <li>down</li>
+            <li>.</li>
+            <li>This</li>
+            <li>is</li>
+            <li>super</li>
+            <li>large</li>
+            <li>so</li>
+            <li>you</li>
+            <li>can</li>
+            <li>scroll</li>
+            <li>down</li>
+            <li>.</li>
+            <li>This</li>
+            <li>is</li>
+            <li>super</li>
+            <li>large</li>
+            <li>so</li>
+            <li>you</li>
+            <li>can</li>
+            <li>scroll</li>
+            <li>down</li>
+            <li>.</li>
+            <li>This</li>
+            <li>is</li>
+            <li>super</li>
+            <li>large</li>
+            <li>so</li>
+            <li>you</li>
+            <li>can</li>
+            <li>scroll</li>
+            <li>down</li>
+            <li>.</li>
+          </ul>
         ),
-        displayName: 'Tab 3'
+        displayName: 'Scroll'
       }
     ]}
+    tabsBarStyle={{backgroundColor: 'rgb(160, 214, 249)'}}
   />
 )
 
