68d671bb95cb30b9ff48bcec881d9b60af79dcec
Assumpta Ferrer <Assumpta Ferrer>
2015-12-24 14:10:51 +0100

    global for radium

--- src/__test__/tabs-test.js
+++ src/__test__/tabs-test.js
@@ -1,6 +1,10 @@
 
 'use strict'
 
+/* Global variables for RADIUM */
+global.navigator = {}
+global.navigator.userAgent = 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'
+
 var chai = require('chai')
 var sd = require('skin-deep')
 
